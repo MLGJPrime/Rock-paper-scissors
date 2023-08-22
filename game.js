a8b278ce73f273f90b479ca151cd246b08d0ea26
JPrime <MLGJPrime@users.noreply.github.com>
2023-08-22 16:52:47 +0200

    Added function playRound and set up a game (The output is incorrect)

--- game.js
+++ game.js
@@ -2,8 +2,23 @@ function getComputerChoice() {
     let n = Math.random()*3;
     if (n < 1) {
         return "Rock";
-    } else if (1 < n < 2) {
+    } else if (1 < n <= 2) {
         return "Paper";
-    } else
+    } else {
         return "Scissors";
+    }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if ((computerSelection === "Rock" && playerSelection === "Scissors") || (computerSelection === "Scissors" && playerSelection === "Paper") || (computerSelection === "Paper" || playerSelection === "Rock")) {
+        return "You lose! " + computerSelection + " beats " + playerSelection + ".";
+    } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Paper") || (playerSelection === "Paper" || computerSelection === "Rock")) {
+        return "You win! " + playerSelection + " beats " + computerSelection + ".";
+    } else {
+        return "It's a tie! " + playerSelection + " ties with " + computerSelection + ".";
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
