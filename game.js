b8a4e56110c41db48215ba950242042ab0752c3b
JPrime <jan.predrag2@gmail.com>
2024-02-19 21:27:25 +0100

    Added event listeners to buttons to get player choice and outcome

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-const prompt = require("prompt-sync")();
-
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const index = Math.floor(Math.random() * choices.length);
@@ -24,29 +22,47 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function playGame(numRounds) {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < numRounds; i++) {
-        let playerSelection = prompt("What is your pick? ");
-        playerSelection = playerSelection.toLowerCase();
-        const computerSelection = getComputerChoice();
+// function playGame(numRounds) {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (let i = 0; i < numRounds; i++) {
+//         let playerSelection = prompt("What is your pick? ");
+//         playerSelection = playerSelection.toLowerCase();
+//         const computerSelection = getComputerChoice();
+//         const roundResult = playRound(playerSelection, computerSelection);
+//         console.log(roundResult);
+//         if (roundResult.includes("win")) {
+//             playerScore++;
+//         } else if (roundResult.includes("lose")) {
+//             computerScore++;
+//         }
+//     }
+//     if (playerScore > computerScore) {
+//         console.log("You win the game!");
+//     } else if (playerScore < computerScore) {
+//         console.log("You lose the game!");
+//     } else {
+//         console.log("It's a tie game!");
+//     }
+// }
+
+const buttons = [
+    { selector: "#btn1", value: "rock" },
+    { selector: "#btn2", value: "paper" },
+    { selector: "#btn3", value: "scissors" },
+];
+
+const computerSelection = getComputerChoice();
+
+buttons.forEach(button => {
+    const btn = document.querySelector(button.selector);
+    btn.addEventListener("click", () => {
+        const playerSelection = button.value;
         const roundResult = playRound(playerSelection, computerSelection);
         console.log(roundResult);
-        if (roundResult.includes("win")) {
-            playerScore++;
-        } else if (roundResult.includes("lose")) {
-            computerScore++;
-        }
-    }
-    if (playerScore > computerScore) {
-        console.log("You win the game!");
-    } else if (playerScore < computerScore) {
-        console.log("You lose the game!");
-    } else {
-        console.log("It's a tie game!");
-    }
-}
+    });
+});
 
-let numRounds = prompt("How many rounds do you want to play? ");
-playGame(numRounds);
+
+// let numRounds = prompt("How many rounds do you want to play? ");
+// playGame(numRounds);
